b1756ba9071208c7583667c0906b3e2f0d5ef764
GO-Shoderu <go.shoderu@gmail.com>
2023-08-02 13:34:38 +0200

    Adding a new topic learnt: Variables

---
+++ variables/app.py
@@ -0,0 +1,19 @@
+# defining variables
+price = 10  # integer
+rating = 4.9    # floats
+name = "Gabe"   # string
+is_published = True     # boolean
+is_attached = False     # boolean
+
+# printing the variable
+print(price)
+print(rating)
+print(name)
+print(is_published)
+print(is_attached)
+
+# exercise by Mosh
+patient_name = "John Smith"
+patient_age = 20
+patient_status = "new"
+
