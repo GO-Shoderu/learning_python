a7939d686cef52639388f6920d92b00ee8fc352b
GO-Shoderu <go.shoderu@gmail.com>
2023-08-03 15:02:07 +0200

    Updating: /input/app.py

---
+++ input/app.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+print("Hi " + name + ", python is fun!!!")
+
+# exercise
+print("Okay, let's try this again")
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+print(name + " likes " + color)
