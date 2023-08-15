4dea765999146f9dcfa7884f29e7fdbb52f88862
GO-Shoderu <go.shoderu@gmail.com>
2023-08-15 14:06:30 +0200

    Adding a new topic: Tuple

---
+++ tuples/README.md
@@ -0,0 +1,6 @@
+Working with Tuples
+
+unlike arrays it is immutable
+aside from magic methods it comes with only 2 methods
+count - to check for an occurence of an item in a tuple
+index - to check for the index of an item in a tuple`
---
+++ tuples/app.py
@@ -0,0 +1,17 @@
+#unlike arrays, tuples are immutable, meaning you can reassign tuples after creation
+#creating a tuple
+numbers = (1,2,3)
+
+#printing all the items in a tuple
+print(f"All the contents of the tuple are {numbers}")
+
+#printing an item in a tuple
+print(f"The content in position 0 is {numbers[0]}")
+
+#checking the number of occurence of a content in a tuple
+print(f"The number of itmes in this tuple is {numbers.count(2)}")
+
+#checking for the index where an item resides
+print(f"The value 2 is at index {numbers.index(2)}")
+
+#aside from the magic methods, these are the only 2 methods that can be used in tuples
